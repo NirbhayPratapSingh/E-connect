d3512bfdb1a3055444ef80f6b9521fbcf1886dfe
Devesh Goyal <Devesh26goyal@gmail.com>
2022-07-28 15:09:14 +0530

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Components/HomePage/HomePage';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-[10rem]">HomePage</div>;
+  return <div className="text-[1rem]">HomePage</div>;
 };
 
 export default HomePage;
