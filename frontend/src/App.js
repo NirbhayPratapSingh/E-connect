66f525d710189077b735687f8c673f8b301e9ffb
P Naga <leadernaga90@gmail.com>
2022-07-28 15:25:16 +0530

    home signin sginup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './Components/HomePage/HomePage';
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './Components/HomePage/HomePage'
+import Login from './Components/Login/Login'
+import Signup from './Components/Login/Signup'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React from 'react'
 
 const HomePage = () => {
-  return <div className="text-[10rem]">HomePage</div>;
-};
+  return (
+    <div class="container px-6 py-16 mx-auto">
+      <div class="items-center lg:flex">
+        <div class="w-full lg:w-1/2">
+          <div class="lg:max-w-lg">
+            <h1 class="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl">
+              Best Place To Find Your Learning Partner
+            </h1>
+            <p class="mt-2 text-gray-600 dark:text-gray-400">
+              learning with others is a manner of group learning that enhances
+              communication skills, cultural awareness, thinking skills and so
+              much more.
+            </p>
+          </div>
+        </div>
 
-export default HomePage;
+        <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+          <video
+            autoPlay
+            loop
+            muted
+            poster="https://lh3.googleusercontent.com/EdrznbVa_EV1flSpEnA__oab0QpVW9h15iWP1BLOvVdwZ1ySw6JCQzYiu4eZA9f_FBHlIRsoURu5kjdOWhdunV0Ep7FIjc5QhPNc9Q=w1180-e365-rw-lo-l100"
+          >
+            <source
+              type="video/webm"
+              src="https://storage.googleapis.com/gweb-aiaz.appspot.com/animations/Masthead.webm"
+            />
+          </video>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
