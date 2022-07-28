6a8e6b641c9f914973b9ff4e1837fb090f8a3196
P Naga <leadernaga90@gmail.com>
2022-07-28 19:05:34 +0530

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,46 @@
-const http = require("http");
-const express = require('express');
-const Connection = require('./server/server');
-const cors = require('cors');
-const { Server } = require("socket.io");
-const cookieParser = require("cookie-parser")
-const Login = require("./Routes/Login")
-const Signup = require("./Routes/Signup")
+const http = require('http')
+const express = require('express')
+const Connection = require('./server/server')
+const cors = require('cors')
+const { Server } = require('socket.io')
+const cookieParser = require('cookie-parser')
+const Login = require('./Routes/Login')
+const Signup = require('./Routes/Signup')
+require('dotenv').config()
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json());
+app.use(express.json())
 app.use(cookieParser())
 
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-    })
-);
+  cors({
+    origin: ['http://localhost:3000'],
+  }),
+)
 
-app.get("/", (req, res) => {
-    res.send("Welcome")
+app.get('/', (req, res) => {
+  res.send('Welcome')
 })
-app.use("/login", Login);
-app.use("/signup",Signup)
+// app.use('/login', Login)
+// app.use('/signup', Signup)
 
 server.listen(port, async (err, res) => {
-    try {
-        await Connection;
-        console.log("Connected Successfully");
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await Connection
+    console.log('Connected Successfully')
+  } catch (err) {
+    console.log(err)
+  }
 
-    if (err) {
-        return console.log("Something went wrong");
-    }
-    console.log("Sever is live at http://localhost:8080");
-});
+  if (err) {
+    return console.log('Something went wrong')
+  }
+  console.log('Sever is live at http://localhost:8080')
+})
