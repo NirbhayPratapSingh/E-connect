8fb3bf33bdbb203c44e5ab0760097c54d310f52a
Devesh Goyal <Devesh26goyal@gmail.com>
2022-07-29 13:30:23 +0530

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,63 @@ const Authorization = require('./Routes/AuthMiddlewere/authMiddlewere')
 
 const port = process.env.PORT || 8080
 
+let users = [];
+let messages = {
+  general: [],
+  random: [],
+  jokes: [],
+  javascript: []
+}
+
+io.on("connection", (socket) => {
+  socket.on("join server", (username) => {
+    const user = {
+      username,
+      id: socket.id,
+    }
+
+    users.push(user)
+    io.emit("new user", users);
+  })
+
+  socket.on("join room", (roomName, cb) => {
+    socket.join(roomName);
+    cb(messages[roomName])
+  })
+
+  socket.on("send message", ({ content, to, sender, chatName, isChannel }) => {
+    if (isChannel) {
+      const payload = {
+        content,
+        chatName,
+        sender,
+      };
+      socket.to(to).emit("new message", payload);
+    }
+    else {
+      const payload = {
+        content,
+        chatName: sender,
+        sender
+      };
+      socket.to(to).emit("new message", payload);
+    }
+
+    if (messages[chatName]) {
+      messages[chatName].push({
+        sender,
+        content
+      });
+    }
+  })
+
+  socket.on("disconnect", () => {
+    users = users.filter(u => u.id !== socket.id);
+    io.emit("new user", users);
+  })
+})
+
+
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
