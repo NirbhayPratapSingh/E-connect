99aa32e325318825877b0a6cbecb7b1db65f8b60
Devesh Goyal <Devesh26goyal@gmail.com>
2022-07-29 13:53:06 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import HomePage from './Components/HomePage/HomePage'
 import Login from './Components/Login/Login'
 import Signup from './Components/Login/Signup'
@@ -7,38 +7,37 @@ import './App.css'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer.jsx/Footer'
 import ErrorPage from './Components/ErrorPage/ErrorPage'
-import { useState } from 'react'
 import { useContext } from 'react'
-import { AuthContext, AuthProvider } from './contextApi/AuthContext'
+import { AuthContext } from './contextApi/AuthContext'
 import Chat from './Components/Chat/Chat'
-import { useNavigate,Navigate } from 'react-router-dom'
+import Main from './Components/Main/Main'
 
 function App() {
   const location = useLocation()
   const { login } = useContext(AuthContext)
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
 
 
   return (
     <div className="bg-white shadow dark:bg-gray-800">
       {location.pathname === '/login' ||
-      location.pathname === '/signup' ||
-      location.pathname === '/chat' ? null : (
+        location.pathname === '/signup' ||
+        location.pathname === '/chat' ? null : (
         <Header />
       )}
 
       <Routes>
-        <Route path="/" element={login ? <HomePage /> : navigate("/login")} />
-        <Route path="/login" element={ <Login /> } />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={login ? <Chat /> : navigate("/login")} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
 
       {location.pathname === '/login' ||
-      location.pathname === '/signup' ||
-      location.pathname == '/chat' ? null : (
+        location.pathname === '/signup' ||
+        location.pathname == '/chat' ? null : (
         <Footer />
       )}
     </div>
